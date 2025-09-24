86d77fd915ff17e7f84a7cc7d832f5ad92b136c5
miquelcabot <miquel.cabot@gmail.com>
2025-09-24 14:47:40 +0200

    Rename from api to zkp module

--- ats/zkp/src/lib.rs
+++ ats/zkp/src/lib.rs
@@ -1,15 +1,15 @@
 use ark_bn254::{Bn254, Fr};
 
-pub mod api;
 pub mod circuit;
 pub mod hashing;
 pub mod utils;
+pub mod zkp;
 
 // Exposed types
 pub type Curve = Bn254;
 pub type F = Fr;
 
-pub use api::*;
 pub use circuit::*;
 pub use hashing::*;
 pub use utils::*;
+pub use zkp::*;
